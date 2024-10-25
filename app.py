ad10aaf9b3a61a05daa84c7680b26649984b3eb3
Jeremy Rummel <42078577+jfrummel@users.noreply.github.com>
2024-10-25 15:02:07 -0500

    Update app.py
    Added them and grid

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ import numpy as np
 from shiny.express import ui, input, render
 from shinyswatch import theme
 
-ui.page_opts(title="My PyShiny App with an Interactive Slider", fillable=True, theme=theme.darkly)
+ui.page_opts(
+    title="My PyShiny App with an Interactive Slider", fillable=True, theme=theme.superhero
+)
 
 with ui.sidebar():
     ui.input_slider("selected_number_of_bins", "Number of Bins", 1, 150, 50)
-
+    
 
 @render.plot(alt="A histogram of number of bins selected")
 def histogram():
     np.random.seed(3)
     x = 100 + 15 * np.random.randn(437)
-    plt.hist(x, input.selected_number_of_bins(),color="purple", density=True)
+    plt.hist(x, input.selected_number_of_bins(), color="purple", density=True)
     plt.grid()
     plt.title("My Histogram")
